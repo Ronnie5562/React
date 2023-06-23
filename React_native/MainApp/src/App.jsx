a3c3f21285ea7e467c6f70c8ddd2a0c60e2cb710
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-06-23 19:05:31 +0200

    Icons

--- React_native/MainApp/src/App.jsx
+++ React_native/MainApp/src/App.jsx
@@ -8,12 +8,30 @@ import Screen_A from './Screen_A';
 import Screen_B from './Screen_B';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
 const Tab = createBottomTabNavigator();
 const App = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, size, color}) => {
+                        let iconName;
+                        if (route.name === 'Screen_A'){
+                            iconName = 'autoprefixer';
+                            
+                        } else if (route.name === 'Screen_B'){
+                            iconName='btc'
+                        }
+                        return (
+                            <FontAwesome5 
+                                name={iconName}
+                            />
+                        )
+                    }
+                })}
+            >
                 <Tab.Screen
                     name='Screen_A'
                     component={Screen_A}
