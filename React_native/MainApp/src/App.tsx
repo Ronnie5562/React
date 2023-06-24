71c370275852dce44b133aae9dde81bee90bddd7
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-06-24 17:48:07 +0200

    Learning in progress.....

--- React_native/MainApp/src/App.tsx
+++ React_native/MainApp/src/App.tsx
@@ -8,6 +8,7 @@ import Screen_A from './Screen_A';
 import Screen_B from './Screen_B';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator();
 const App = () => {
@@ -19,14 +20,27 @@ const App = () => {
                         let iconName;
                         if (route.name === 'Screen_A'){
                             iconName = 'autoprefixer';
-                            
+                            size = focused ? 25 : 20;
+                            color = focused ? '#f0f' : '#555'
                         } else if (route.name === 'Screen_B'){
                             iconName='btc'
+                            size = focused ? 25 : 20;
+                            color = focused ? '#f0f' : '#555'
                         }
                         return (
-                            <Text>Hello</Text>
+                            <FontAwesome5 
+                                name={iconName}
+                                size={size}
+                                color={color}
+                            />
                         )
-                    }
+                    },
+                    "tabBarActiveTintColor": '#f0f',
+                    "tabBarInactiveTintColor": '#555',
+                    "tabBarActiveBackgroundColor": '#fff',
+                    "tabBarInactiveBackgroundColor": '#999',
+                    "tabBarShowLabel": false,
+                    
                 })}
             >
                 <Tab.Screen
