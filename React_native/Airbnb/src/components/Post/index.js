b0533aa8546f19d4f729b23a9d8337f5a267fbd0
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-06-28 09:27:15 +0200

    Learning in progress.....

--- React_native/Airbnb/src/components/Post/index.js
+++ React_native/Airbnb/src/components/Post/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import styles from './styles';
 
 const Post = () => {
   return (
-    <View>
-        <Text>This is post</Text>  
+    <View style={styles.container}>
+        <Image 
+            style={styles.image} 
+            source={{ 
+                uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg'
+            }} 
+        />
+        <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+        <Text style={styles.description} numberOfLines={2}>
+            Lorem Ipsum is simplyLorem Ipsum is simply dummy text.The apartment is in the center of Santa Cruz Lorem Ipsum is simplyLorem Ipsum is simply dummy text
+        </Text>
+        <Text style={styles.prices}>
+            <Text style={styles.oldPrice}>$36 </Text>
+            <Text style={styles.price}> $30 </Text>
+            <Text>/ night</Text>
+        </Text>
+          <Text style={styles.totalPrice}>$230 total</Text>
     </View>
   )
 }
--- React_native/Airbnb/src/components/Post/styles.js
+++ React_native/Airbnb/src/components/Post/styles.js
@@ -1,7 +1,41 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-
+    container: {
+        margin: 20,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 3 / 2,
+        resizeMode: 'cover',
+        borderRadius: 10,
+    },
+    bedrooms: {
+        marginVertical: 10,
+        color: '#5b5b5b'
+    },
+    description: {
+        fontSize: 18,
+        lineHeight: 26,
+        color: '#000'
+    },
+    prices: {
+        fontSize: 18,
+        color: '#000',
+        marginVertical: 10,
+    },
+    oldPrice: {
+        color: '#5b5b5b',
+        textDecorationLine: 'line-through',
+    },
+    price:{
+        fontWeight: '800',
+        color: '#000',
+    },
+    totalPrice: {
+        color: '#5b5b5b',
+        textDecorationLine: 'underline'
+    },
 });
 
 export default styles;
